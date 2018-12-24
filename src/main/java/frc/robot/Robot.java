f7638254e354dfd3986c77702a4f84175c572a7d
AvidhBavkar <avidhbavkar@gmail.com>
2018-12-23 19:10:02 -0800

    Cleaned + Documented Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,6 @@
 package frc.robot;
 
-import java.io.File;
 import java.util.LinkedHashMap;
-
-import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -13,68 +10,81 @@ import frc.robot.auton.PathLoader;
 import frc.robot.auton.Ramsete;
 import frc.robot.subsystems.Drivetrain;
 import frc.util.CheesyDriveHelper;
-import frc.util.kinematics.pos.RobotPos;
-import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 
-public class Robot extends IterativeRobot{;
+public class Robot extends IterativeRobot{
 
+    //Teleoperated Mode Controller;
     CheesyDriveHelper mCheesyDriveHelper = new CheesyDriveHelper();
 
+
+    //THREAD TIMESTEPS (in seconds):
     public static final double kLocalizerTimestep = 0.005;
     public static final double kLoggingTimestep = 0.1;
     public static final double kRamseteTimestep = 0.01;
 
+    //Map of all autonomous paths
     public static LinkedHashMap<String, Trajectory> autonPaths;
 
+    //The selected autonomous command to run.
     Command autonCommand;
 
+    //Drive Camera
     // private UsbCamera driveCam;
 
     @Override
     public void robotInit(){
 
+        //Drive Camera Initialization:
         // driveCam = CameraServer.getInstance().startAutomaticCapture();
 		// driveCam.setResolution(320, 240);
         // driveCam.setFPS(12);
+        // CameraServer.getInstance().startAutomaticCapture();
 
+        //Zero All Subsystems:
         Drivetrain.getInstance().zeroSensor();
 
         //verify both Odometery and Ramsete are constructed
-        // Odometery.getInstance();
         Ramsete.getInstance();
         PeriodicLogger.getInstance();
 
-        //Add Loggable to Periodic Logger
+
+        //Import all autonomous paths from filesystem (time intensive)
+        autonPaths = PathLoader.loadPaths();
+
+        //select the autonomous command
+        //in competition this will likely be done in autonomousInit()
+        autonCommand = new MoveCommand(autonPaths.get("DriveLeftCurve"));
+
+
+        //Add Loggables to Periodic Logger
         PeriodicLogger.addLoggable(Drivetrain.getInstance());
         PeriodicLogger.addLoggable(Ramsete.getInstance());
 
-        CameraServer.getInstance().startAutomaticCapture();
-
-        autonPaths = PathLoader.loadPaths();
-
-        autonCommand = new MoveCommand(autonPaths.get("DriveLeftCurve"));
-
+        //Start the Periodic Logger.
         PeriodicLogger.getInstance().start();
     }
 
     @Override
     public void autonomousInit() {
         intoEnabled();
+
+        //Zero relevant subsystems:
         Drivetrain.getInstance().zeroSensor();
 
+        //Start an Odometery thread to begin keeping track of the vehicle position
         Drivetrain.getInstance().startOdometery(kLocalizerTimestep);
+        
+        //Start the autonomous controller
         Ramsete.getInstance().start();
 
-        // Ramsete.getInstance().trackPath(autonPaths.get("LeftCurve"));
-
+        //Start the selected autonomous command.
         autonCommand.start();
     }
 
     @Override
     public void autonomousPeriodic() {
         Scheduler.getInstance().run();
-        // System.out.println(autonPaths.get("DriveLeftCurve"));
     }
     
     @Override
@@ -84,6 +94,8 @@ public class Robot extends IterativeRobot{;
 
     @Override
     public void teleopPeriodic() {
+
+        //Update Inputs and Outputs
         Drivetrain.getInstance().setRawSpeed(
             mCheesyDriveHelper.cheesyDrive(
             OI.getInstance().getForward(),
@@ -91,10 +103,6 @@ public class Robot extends IterativeRobot{;
             OI.getInstance().getQuickTurn()
             , true)
         );
-    }
-
-    @Override
-    public void robotPeriodic() {
     }
 
     @Override
